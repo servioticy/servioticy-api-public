a1dfa37c730a1ac3b8e7f333bdc81aeb87dbe670
jlperez <juanluis@ac.upc.edu>
2014-02-11 12:15:05 +0100

    Added getting Service Objects

--- src/main/java/es/bsc/servioticy/api/Paths.java
+++ src/main/java/es/bsc/servioticy/api/Paths.java
@@ -1,3 +1,18 @@
+/*******************************************************************************
+ * Copyright 2014 Barcelona Supercomputing Center (BSC)
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
 package es.bsc.servioticy.api;
 
 import java.net.URI;
@@ -8,6 +23,7 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.HttpHeaders;
@@ -19,7 +35,6 @@ import javax.ws.rs.core.UriInfo;
 import es.bsc.servioticy.api_commons.data.CouchBase;
 import es.bsc.servioticy.api_commons.data.SO;
 import es.bsc.servioticy.api_commons.exceptions.ServIoTWebApplicationException;
-import es.bsc.servioticy.api_commons.utils.Authorization;
 
 
 @Path("/public")
@@ -28,22 +43,10 @@ public class Paths {
   @Context
   private transient HttpServletRequest servletRequest;
   
-  @GET
-  @Path("/hola")
-  @Produces("text/plain")
-  public String getHello(@Context HttpHeaders hh) {
-    
-//    // Check Authorization header
-//    Authorization aut = new Authorization();
-//    aut.checkAuthorization(hh.getRequestHeaders());
-
-    return "Holaaaaaa";
-  }
-
   @POST
   @Produces("application/json")
   @Consumes(MediaType.APPLICATION_JSON)
-  public Response createThng(@Context HttpHeaders hh, String body) {
+  public Response createSO(@Context HttpHeaders hh, String body) {
     
     String user_id = (String) this.servletRequest.getAttribute("user_id");
     
@@ -58,19 +61,35 @@ public class Paths {
     CouchBase cb = new CouchBase();
     cb.setSO(so);
     
-    // Construct the access thng URI
-    String thngUri = uriInfo.getAbsolutePath().toString();
-    thngUri = thngUri.substring(0, thngUri.lastIndexOf('/')); //Uri without .../create.json
-    UriBuilder ub = UriBuilder.fromPath(thngUri);
-    URI userUri = ub.path(so.getId())
-            .build();
+    UriBuilder ub = uriInfo.getAbsolutePathBuilder();
+    URI soUri = ub.path(so.getId()).build();
     
-    return Response.created(userUri)
+    return Response.created(soUri)
              .entity(so.getString())
              .header("Server", "api.compose")
              .header("Date", new Date(System.currentTimeMillis()))
              .build();
   }
 
+  
+  @Path("/{soId}")
+  @GET
+  @Produces("application/json")
+  public Response getSO(@Context HttpHeaders hh, @PathParam("soId") String so_id) {
 
+    String user_id = (String) this.servletRequest.getAttribute("user_id");
+    
+    // Get the Service Object
+    CouchBase cb = new CouchBase();
+    SO storedSO = cb.getSO(user_id, so_id);
+    
+    if (storedSO == null)
+      throw new ServIoTWebApplicationException(Response.Status.NOT_FOUND, "The Service Object was not found.");
+    
+    return Response.ok(storedSO.getString())
+           .header("Server", "api.compose")
+           .header("Date", new Date(System.currentTimeMillis()))
+           .build();
+  }
+  
 }
