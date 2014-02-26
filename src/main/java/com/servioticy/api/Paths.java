e27d87fe6dafccd2edf99ff2153dc41e28624d31
jlperez <juanluis@ac.upc.edu>
2014-02-26 12:46:11 +0100

    Create, update and fix few issues

--- src/main/java/com/servioticy/api/Paths.java
+++ src/main/java/com/servioticy/api/Paths.java
@@ -35,52 +35,36 @@ import javax.ws.rs.core.UriBuilder;
 import javax.ws.rs.core.UriInfo;
 
 import com.servioticy.api.commons.data.CouchBase;
-import com.servioticy.api.commons.data.Group;
 import com.servioticy.api.commons.data.SO;
 import com.servioticy.api.commons.data.Subscription;
 import com.servioticy.api.commons.datamodel.Data;
 import com.servioticy.api.commons.exceptions.ServIoTWebApplicationException;
+import com.servioticy.api.commons.utils.Authorization;
 import com.servioticy.api.commons.utils.Config;
 import com.servioticy.queueclient.QueueClient;
 import com.servioticy.queueclient.QueueClientException;
 
 
-@Path("/public")
+@Path("/")
 public class Paths {
   @Context UriInfo uriInfo;
   @Context
   private transient HttpServletRequest servletRequest;
   
   
-  @GET
-  @Produces("application/json")
-  @Consumes(MediaType.APPLICATION_JSON)
-  public Response getAllSOs(@Context HttpHeaders hh) {
-    
-    String user_id = (String) this.servletRequest.getAttribute("user_id");
-    
-    CouchBase cb = new CouchBase();
-    String sos = cb.getAllSOs(user_id);
-    
-    return Response.ok(sos)
-           .header("Server", "api.compose")
-           .header("Date", new Date(System.currentTimeMillis()))
-           .build();
-  }  
-  
   @POST
   @Produces("application/json")
   @Consumes(MediaType.APPLICATION_JSON)
   public Response createSO(@Context HttpHeaders hh, String body) {
     
-    String user_id = (String) this.servletRequest.getAttribute("user_id");
+    String userId = (String) this.servletRequest.getAttribute("userId");
     
     // Check if exists request data
     if (body.isEmpty())
       throw new ServIoTWebApplicationException(Response.Status.BAD_REQUEST, "No data in the request");
     
     // Create the Service Object
-    SO so = new SO(user_id, body);
+    SO so = new SO(userId, body);
     
     // Store in Couchbase
     CouchBase cb = new CouchBase();
@@ -91,29 +75,211 @@ public class Paths {
     URI soUri = ub.path(so.getId()).build();
     
     return Response.created(soUri)
-             .entity(so.getString())
-             .header("Server", "api.compose")
+             .entity(so.responseCreateSO())
+					   .header("Server", "api.servIoTicy")
              .header("Date", new Date(System.currentTimeMillis()))
              .build();
+  }
+
+  @GET
+  @Produces("application/json")
+  @Consumes(MediaType.APPLICATION_JSON)
+  public Response getAllSOs(@Context HttpHeaders hh) {
+    
+    String userId = (String) this.servletRequest.getAttribute("userId");
+    
+    CouchBase cb = new CouchBase();
+    String sos = cb.getAllSOs(userId);
+    
+    return Response.ok(sos)
+					   .header("Server", "api.servIoTicy")
+           .header("Date", new Date(System.currentTimeMillis()))
+           .build();
   }
 
   @Path("/{soId}")
   @GET
   @Produces("application/json")
-  public Response getSO(@Context HttpHeaders hh, @PathParam("soId") String so_id) {
+  public Response getSO(@Context HttpHeaders hh, @PathParam("soId") String soId) {
 
-    String user_id = (String) this.servletRequest.getAttribute("user_id");
+    Authorization aut = (Authorization) this.servletRequest.getAttribute("aut");
     
     // Get the Service Object
     CouchBase cb = new CouchBase();
-    SO storedSO = cb.getSO(user_id, so_id);
-    if (storedSO == null)
+    SO so = cb.getSO(soId);
+    if (so == null)
       throw new ServIoTWebApplicationException(Response.Status.NOT_FOUND, "The Service Object was not found.");
     
-    return Response.ok(storedSO.getString())
-           .header("Server", "api.compose")
-           .header("Date", new Date(System.currentTimeMillis()))
-           .build();
+    // check authorization -> same user and not public
+    aut.checkAuthorization(so);
+    
+    return Response.ok(so.responseGetSO())
+					   .header("Server", "api.servIoTicy")
+					   .header("Date", new Date(System.currentTimeMillis()))
+					   .build();
+  }
+
+  @Path("/{soId}/streams")
+  @GET
+  @Produces("application/json")
+  public Response getStreams(@Context HttpHeaders hh, @PathParam("soId") String soId) {
+  
+    Authorization aut = (Authorization) this.servletRequest.getAttribute("aut");
+  	
+  	// Get the Service Object
+  	CouchBase cb = new CouchBase();
+  	SO so = cb.getSO(soId);
+  	if (so == null)
+  		throw new ServIoTWebApplicationException(Response.Status.NOT_FOUND, "The Service Object was not found.");
+  	
+    // check authorization -> same user and not public
+    aut.checkAuthorization(so);
+
+    // Generate response
+    String response = so.responseStreams();
+    
+    if (response == null)
+      return Response.noContent()
+					   .header("Server", "api.servIoTicy")
+					   .header("Date", new Date(System.currentTimeMillis()))
+					   .build();
+		
+		return Response.ok(response)
+					   .header("Server", "api.servIoTicy")
+					   .header("Date", new Date(System.currentTimeMillis()))
+					   .build();
+  }
+
+  @Path("/{soId}/streams/{streamId}")
+  @PUT
+  @Produces("application/json")
+  public Response updateSOData(@Context HttpHeaders hh, @PathParam("soId") String soId, 
+                    @PathParam("streamId") String streamId, String body) {
+  
+    Authorization aut = (Authorization) this.servletRequest.getAttribute("aut");
+  	
+    // Check if exists request data
+    if (body.isEmpty())
+      throw new ServIoTWebApplicationException(Response.Status.BAD_REQUEST, "No data in the request");
+
+  	// Get the Service Object
+  	CouchBase cb = new CouchBase();
+  	SO so = cb.getSO(soId);
+  	if (so == null)
+  		throw new ServIoTWebApplicationException(Response.Status.NOT_FOUND, "The Service Object was not found.");
+  	
+    // check authorization -> same user and not public
+    aut.checkAuthorization(so);
+    
+    // Create Data
+    Data data = new Data(so, streamId, body);
+  
+    // Generate opId
+    String opId = UUID.randomUUID().toString().replaceAll("-", "");
+  
+    // Queueing
+    QueueClient sqc;
+    try {
+      sqc = QueueClient.factory();
+      sqc.connect();
+      boolean res = sqc.put("{\"opid\": \"" + opId + "\", \"soid\": \"" + soId + 
+          "\", \"streamid\": \"" + streamId + "\", \"su\": " + body + "}");
+      if (!res) {
+        // TODO -> what to do with res = false
+      }
+      sqc.disconnect();
+  
+    } catch (QueueClientException e) {
+      throw new ServIoTWebApplicationException(Response.Status.INTERNAL_SERVER_ERROR, 
+          "SQueueClientException " + e.getMessage());
+    } catch (Exception e) {
+      throw new ServIoTWebApplicationException(Response.Status.INTERNAL_SERVER_ERROR, 
+          "Undefined error in SQueueClient");
+    }
+    
+    // Store in Couchbase
+    cb.setData(data);
+    
+    // Set the opId
+    cb.setOpId(opId, Config.getOpIdExpiration());
+    
+    return Response.ok(body)
+					   .header("Server", "api.servIoTicy")
+             .header("Date", new Date(System.currentTimeMillis()))
+             .build();
+  }
+
+  @Path("/{soId}/streams/{streamId}")
+  @GET
+  @Produces("application/json")
+  public Response getSOData(@Context HttpHeaders hh, @PathParam("soId") String soId, 
+                    @PathParam("streamId") String streamId) {
+  
+    Authorization aut = (Authorization) this.servletRequest.getAttribute("aut");
+  	
+  	// Get the Service Object
+  	CouchBase cb = new CouchBase();
+  	SO so = cb.getSO(soId);
+  	if (so == null)
+  		throw new ServIoTWebApplicationException(Response.Status.NOT_FOUND, "The Service Object was not found.");
+  	
+    // check authorization -> same user and not public
+    aut.checkAuthorization(so);
+    
+    // Get the Service Object Data
+    Data data = cb.getData(so, streamId);
+
+    if (data == null)
+      return Response.noContent()
+					   .header("Server", "api.servIoTicy")
+					   .header("Date", new Date(System.currentTimeMillis()))
+					   .build();
+    
+    // Generate response
+    String response = data.responseAllData();
+  
+    if (response == null)
+      return Response.noContent()
+					   .header("Server", "api.servIoTicy")
+					   .header("Date", new Date(System.currentTimeMillis()))
+					   .build();
+		
+    return Response.ok(response)
+					   .header("Server", "api.servIoTicy")
+             .header("Date", new Date(System.currentTimeMillis()))
+             .build();
+  }
+
+  @Path("/{soId}/streams/{streamId}/lastUpdate")
+  @GET
+  @Produces("application/json")
+  public Response getLastUpdate(@Context HttpHeaders hh, @PathParam("soId") String soId,
+                    @PathParam("streamId") String streamId) {
+
+    Authorization aut = (Authorization) this.servletRequest.getAttribute("aut");
+
+  	// Get the Service Object
+  	CouchBase cb = new CouchBase();
+  	SO so = cb.getSO(soId);
+  	if (so == null)
+  		throw new ServIoTWebApplicationException(Response.Status.NOT_FOUND, "The Service Object was not found.");
+  	
+    // check authorization -> same user and not public
+    aut.checkAuthorization(so);
+    
+    // Get the Service Object Data
+    Data data = cb.getData(so, streamId);
+  
+    if (data == null)
+      return Response.noContent()
+					   .header("Server", "api.servIoTicy")
+					   .header("Date", new Date(System.currentTimeMillis()))
+					   .build();
+		
+    return Response.ok(data.responseLastUpdate())
+					   .header("Server", "api.servIoTicy")
+             .header("Date", new Date(System.currentTimeMillis()))
+             .build();
   }
 
   @Path("/{soId}/streams/{streamId}/subscriptions")
@@ -123,17 +289,25 @@ public class Paths {
   public Response createSubscription(@Context HttpHeaders hh, @PathParam("soId") String soId , 
                     @PathParam("streamId") String streamId, String body) {
 
-    String user_id = (String) this.servletRequest.getAttribute("user_id");
+    Authorization aut = (Authorization) this.servletRequest.getAttribute("aut");
     
     // Check if exists request data
     if (body.isEmpty())
       throw new ServIoTWebApplicationException(Response.Status.BAD_REQUEST, "No data in the request");
+
+  	// Get the Service Object
+  	CouchBase cb = new CouchBase();
+  	SO so = cb.getSO(soId);
+  	if (so == null)
+  		throw new ServIoTWebApplicationException(Response.Status.NOT_FOUND, "The Service Object was not found.");
+  	
+    // check authorization -> same user and not public
+    aut.checkAuthorization(so);
     
     // Create Subscription
-    Subscription subs = new Subscription(user_id, soId, streamId, body);
+    Subscription subs = new Subscription(so, streamId, body);
     
     // Store in Couchbase
-    CouchBase cb = new CouchBase();
     cb.setSubscription(subs);
     
     // Construct the access subscription URI
@@ -141,8 +315,8 @@ public class Paths {
     URI subsUri = ub.path(subs.getId()).build();
 
     return Response.created(subsUri)
-             .entity(subs.getString())
-             .header("Server", "api.compose")
+             .entity(subs.responseCreateSubs())
+					   .header("Server", "api.servIoTicy")
              .header("Date", new Date(System.currentTimeMillis()))
              .build();
   } 
@@ -153,159 +327,29 @@ public class Paths {
 	public Response getSubscriptions(@Context HttpHeaders hh, @PathParam("soId") String soId,
 										@PathParam("streamId") String streamId) {
 
-    String user_id = (String) this.servletRequest.getAttribute("user_id");
+    Authorization aut = (Authorization) this.servletRequest.getAttribute("aut");
 		
 		// Get the Service Object
 		CouchBase cb = new CouchBase();
-		SO so = cb.getSO(user_id, soId);
+		SO so = cb.getSO(soId);
 		if (so == null)
 			throw new ServIoTWebApplicationException(Response.Status.NOT_FOUND, "The Service Object was not found.");
+  	
+    // check authorization -> same user and not public
+    aut.checkAuthorization(so);
+    
+    // Generate response
+    String response = so.responseSubscriptions(streamId);
+    
+    if (response == null)
+      return Response.noContent()
+					   .header("Server", "api.servIoTicy")
+					   .header("Date", new Date(System.currentTimeMillis()))
+					   .build();
 		
-		return Response.ok(so.responseSubscriptions(streamId))
-					   .header("Server", "api.compose")
+		return Response.ok(response)
+					   .header("Server", "api.servIoTicy")
 					   .header("Date", new Date(System.currentTimeMillis()))
 					   .build();
 	}
-	
-	@Path("/{soId}/streams")
-	@GET
-	@Produces("application/json")
-	public Response getStreams(@Context HttpHeaders hh, @PathParam("soId") String soId) {
-
-    String user_id = (String) this.servletRequest.getAttribute("user_id");
-		
-		// Get the Service Object
-		CouchBase cb = new CouchBase();
-		SO so = cb.getSO(user_id, soId);
-		if (so == null)
-			throw new ServIoTWebApplicationException(Response.Status.NOT_FOUND, "The Service Object was not found.");
-		
-		return Response.ok(so.responseStreams())
-					   .header("Server", "api.compose")
-					   .header("Date", new Date(System.currentTimeMillis()))
-					   .build();
-	}
-
-  @Path("/{soId}/streams/{streamId}")
-  @PUT
-  @Produces("application/json")
-  public Response updateSOData(@Context HttpHeaders hh, @PathParam("soId") String soId, 
-                    @PathParam("streamId") String streamId, String body) {
-
-    String user_id = (String) this.servletRequest.getAttribute("user_id");
-		
-    // Check if exists request data
-    if (body.isEmpty())
-      throw new ServIoTWebApplicationException(Response.Status.BAD_REQUEST, "No data in the request");
-    
-    // Create Data
-    Data data = new Data(user_id, soId, streamId, body);
-
-    // Generate opId
-    String opId = UUID.randomUUID().toString().replaceAll("-", "");
-
-    // Queueing
-    QueueClient sqc;
-    try {
-      sqc = QueueClient.factory();
-      sqc.connect();
-      boolean res = sqc.put("{\"opid\": " + opId + ", \"soid\": " + soId + 
-          ", \"streamid\": " + streamId + ", \"su\": " + body);
-      if (!res) {
-        // TODO -> what to do with res = false
-      }
-      sqc.disconnect();
-
-    } catch (QueueClientException e) {
-      throw new ServIoTWebApplicationException(Response.Status.INTERNAL_SERVER_ERROR, 
-          "SQueueClientException " + e.getMessage());
-    } catch (Exception e) {
-      throw new ServIoTWebApplicationException(Response.Status.INTERNAL_SERVER_ERROR, 
-          "Undefined error in SQueueClient");
-    }
-    
-    // Store in Couchbase
-		CouchBase cb = new CouchBase();
-    cb.setData(data);
-    
-    // Set the opId
-    cb.setOpId(opId, Config.getOpIdExpiration());
-    
-    return Response.ok(body)
-             .header("Server", "api.compose")
-             .header("Date", new Date(System.currentTimeMillis()))
-             .build();
-  }
-  
-  @Path("/{soId}/streams/{streamId}/lastUpdate")
-  @GET
-  @Produces("application/json")
-  public Response getLastUpdate(@Context HttpHeaders hh, @PathParam("soId") String soId,
-                    @PathParam("streamId") String streamId) {
-
-    String user_id = (String) this.servletRequest.getAttribute("user_id");
-    
-    // Get the Service Object
-    CouchBase cb = new CouchBase();
-    Data data = cb.getData(user_id, soId, streamId);
-
-    if (data == null)
-      throw new ServIoTWebApplicationException(Response.Status.NOT_FOUND, "No data in the stream of the Service Object.");
-    
-    return Response.ok(data.lastUpdate().toString())
-             .header("Server", "api.compose")
-             .header("Date", new Date(System.currentTimeMillis()))
-             .build();
-  }
-  
-  @Path("/groups/lastUpdate")
-  @POST
-  @Produces("application/json")
-  public Response getLastGroupUpdate(@Context HttpHeaders hh, String body) {
-
-    String user_id = (String) this.servletRequest.getAttribute("user_id");
-    
-    // Check if exists request data
-    if (body.isEmpty())
-      throw new ServIoTWebApplicationException(Response.Status.BAD_REQUEST, "No data in the request");
-    
-    // Create Group petition
-    Group group = new Group(user_id, body);
-    
-//    if (group == null)
-//      throw new ServIoTWebApplicationException(Response.Status.NOT_FOUND, "No data in the stream of the Service Object.");
-    
-    return Response.ok(group.lastUpdate().toString())
-             .header("Server", "api.compose")
-             .header("Date", new Date(System.currentTimeMillis()))
-             .build();
-  }
-
-  @Path("/{soId}/streams/{streamId}/{opId}")
-  @PUT
-  @Produces("application/json")
-  public Response updateInternalSOData(@Context HttpHeaders hh, @PathParam("soId") String soId, 
-                    @PathParam("streamId") String streamId, @PathParam("opId") String opId, String body) {
-
-    String user_id = (String) this.servletRequest.getAttribute("user_id");
-    
-    // Check if exists request data
-    if (body.isEmpty())
-      throw new ServIoTWebApplicationException(Response.Status.BAD_REQUEST, "No data in the request");
-    
-    // Create Data
-    Data data = new Data(user_id, soId, streamId, body);
-
-    // Store in Couchbase
-    CouchBase cb = new CouchBase();
-    cb.setData(data);
-    
-    // Set the opId
-    cb.setOpId(opId, Config.getOpIdExpiration());
-    
-    return Response.ok(body)
-             .header("Server", "api.compose")
-             .header("Date", new Date(System.currentTimeMillis()))
-             .build();
-  }
 }
